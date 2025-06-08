3a3a8bcc5f642747cd4057c9a04f3d24b6851194
= <=>
2025-06-08 11:43:13 +0530

    handle multiple comma-separated numbers

--- src/utils/stringCalculator.test.ts
+++ src/utils/stringCalculator.test.ts
@@ -10,4 +10,7 @@ describe('String Calculator', () => {
   it('returns sum of two comma-separated numbers', () => {
     expect(add('1,2')).toBe(3);
   });
+  it('returns sum of multiple comma-separated numbers', () => {
+    expect(add('1,2,3,4')).toBe(10);
+  });
 });
