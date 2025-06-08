3244e9007806ab3774b0ab2b76f345c5ceea6727
= <=>
2025-06-08 11:38:02 +0530

    return number itself when a single number is passed

--- src/utils/stringCalculator.test.ts
+++ src/utils/stringCalculator.test.ts
@@ -4,4 +4,7 @@ describe('String Calculator', () => {
   it('should return 0 for an empty string', () => {
     expect(add('')).toBe(0);
   });
+  it('returns the number itself for single number input', () => {
+    expect(add('5')).toBe(5);
+  });
 });
--- src/utils/stringCalculator.ts
+++ src/utils/stringCalculator.ts
@@ -1,4 +1,6 @@
 export function add(numbers: string): number {
   // If the input is an empty string, return 0
   if (numbers.trim() === '') return 0;
+  // If the input is a single number, return that number
+  return parseInt(numbers);
 }
